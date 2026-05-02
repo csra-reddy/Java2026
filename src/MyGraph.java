0ed93498f1eeef4c7e1c3ceeb45601c3a399642c
Revathi Vaka <revathi@versa-networks.com>
2026-05-02 09:26:00 -0700

    Graphs BFS

--- src/MyGraph.java
+++ src/MyGraph.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class MyGraph {
 
@@ -19,8 +16,27 @@ public class MyGraph {
         adjList.get(w).add(v);
     }
 
-    List<Integer> getAdjList(int v) {
+    List<Integer> getConnectedNodes(int v) {
         return adjList.get(v);
+    }
+
+    void bfs(int start) {
+
+        Queue<Integer> queue = new ArrayDeque<>();
+        Set<Integer> visited = new HashSet<>();
+        queue.add(start);
+        visited.add(start);
+
+        while(!queue.isEmpty()) {
+            int currentNode = queue.poll();
+            System.out.println(currentNode + " ");
+            for(int i : adjList.get(currentNode)) {
+                if(!visited.contains(i)) {
+                    visited.add(i);
+                    queue.add(i);
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -29,10 +45,14 @@ public class MyGraph {
         graph.addEdge(0, 2);
         graph.addEdge(1, 3);
         graph.addEdge(2, 3);
+        graph.addEdge(3, 4);
+        graph.addEdge(2, 4);
 
-        System.out.println("0" + "-->" +graph.getAdjList(0));
-        System.out.println("1" + "-->" +graph.getAdjList(1));
-        System.out.println("2" + "-->" +graph.getAdjList(2));
-        System.out.println("3" + "-->" +graph.getAdjList(3));
+        System.out.println("0" + "-->" +graph.getConnectedNodes(0));
+        System.out.println("1" + "-->" +graph.getConnectedNodes(1));
+        System.out.println("2" + "-->" +graph.getConnectedNodes(2));
+        System.out.println("3" + "-->" +graph.getConnectedNodes(3));
+        System.out.println("4" + "-->" +graph.getConnectedNodes(4));
+        graph.bfs(0);
     }
 }
